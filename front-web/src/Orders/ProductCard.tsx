83d2d3af7c27a31f4426eb4dfd2e7bbf4e638afa
AguinaldoChaves <a.fabianochaves@gmail.com>
2021-01-10 23:00:55 -0300

    Finish front-web

--- front-web/src/Orders/ProductCard.tsx
+++ front-web/src/Orders/ProductCard.tsx
@@ -1,20 +1,17 @@
 
+import { formatPrice } from './Helpers';
 import { Product } from './types';
 
 type Props = {
     product: Product;
+    onSelectProduct: (product : Product) => void;
+    isSelected: boolean;
 }
-function formatPrice(price: number){
-    const formater = new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        currency: 'BRL',
-        minimumFractionDigits: 2
-    })
-    return formater.format(price);
-}
-function ProductCard({product}:Props){
+
+function ProductCard({product, onSelectProduct, isSelected}:Props){
     return(
-        <div className="order-card-container">
+        <div className={`order-card-container ${isSelected ? 'selected': ''}`}
+        onClick={()=> onSelectProduct(product)}>
             {product.name}
             <img src={product.imageUri} className="order-card-image" alt={product.name}/>
             <h3 className="order-card-price">
--- front-web/src/Orders/ProductsList.tsx
+++ front-web/src/Orders/ProductsList.tsx
@@ -1,14 +1,22 @@
+import { checkIsSelected } from "./Helpers";
 import ProductCard from "./ProductCard";
 import { Product } from "./types";
 type Props ={
     products: Product[];
+    onSelectProduct: (product : Product) => void;
+    selectedProducts:  Product[];
 }
-function ProductsList({ products }:Props){
+function ProductsList({ products, onSelectProduct, selectedProducts}:Props){
     return(
         <div className="orders-list-container">
             <div className="orders-list-items">
                 {products.map(product =>(
-                    <ProductCard key={product.id} product={product}/>
+                    <ProductCard
+                    isSelected={checkIsSelected(selectedProducts, product)}
+                     key={product.id} 
+                     product={product}
+                     onSelectProduct={onSelectProduct}
+                     />
                 ))}
             </div>            
         </div>
